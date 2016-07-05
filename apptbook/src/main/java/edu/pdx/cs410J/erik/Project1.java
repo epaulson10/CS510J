48aa3c1f0d2614c3a387b12572f2b2b844d1619a
Erik Paulson <epaulson10@gmail.com>
2016-07-04 18:38:23 -0700

    Add some comments

--- apptbook/src/main/java/edu/pdx/cs410J/erik/Project1.java
+++ apptbook/src/main/java/edu/pdx/cs410J/erik/Project1.java
@@ -1,13 +1,11 @@
 package edu.pdx.cs410J.erik;
 
 import edu.pdx.cs410J.AbstractAppointmentBook;
-import sun.util.resources.cldr.ar.CalendarData_ar_YE;
 
 import java.text.ParsePosition;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 /**
  * The main class for the CS410J appointment book Project
@@ -16,6 +14,7 @@ public class Project1 {
   public static final String README = "By: Erik Paulson\n" +
           "CS410J Project 1: Designing an Appointment Book Application \n" +
           "A simple command line program that creates an AppointmentBook and Appointment with the given command line arguments.";
+
   public static final String DATE_WRONG_ERROR = "Date is not in 24-hour format: mm/dd/yyyy hh:mm";
 
 
@@ -28,15 +27,22 @@ public class Project1 {
       System.exit(1);
     }
 
+    // Convert the array to an ArrayList to work with the data easier
     ArrayList<String> argList = new ArrayList<>(Arrays.asList(args));
+
+    // If -readme is passed, only print the README and do no more
     if (argList.contains("-readme")) {
       System.out.println(README);
       System.exit(0);
     }
+
+    // If the print option is passed, save that info for later and remove that param
     if (argList.contains("-print")) {
       printDescription = true;
       argList.remove("-print");
     }
+
+    // Validate the number of args
     if (argList.size() != 4) {
       System.err.println("Incorrect command line arguments");
       System.exit(1);
@@ -45,6 +51,7 @@ public class Project1 {
     String description = argList.get(1);
     String start = argList.get(2);
     String end = argList.get(3);
+
     if (description.equals("")) {
       System.err.println("Description cannot be empty");
       System.exit(1);
@@ -55,6 +62,7 @@ public class Project1 {
       System.exit(1);
     }
 
+    // Create an AppointmentBook and Appointment based off of the command line arguments
     AppointmentBook book = new AppointmentBook(owner);
     Appointment appointment = new Appointment(description, start, end);
     book.addAppointment(appointment);
@@ -63,9 +71,16 @@ public class Project1 {
       System.out.println(appointment.toString());
     }
 
-    System.exit(1);
+    System.exit(0);
   }
 
+  /**
+   * A helper function to validate the date passed in from the command line.
+   * A valid date is of the form MM/dd/yyyy HH:mm
+   *
+   * @param date The date to validate
+   * @return true if the date is valid, false otherwise
+     */
   private static boolean isDateValid(String date) {
     // mm/dd/yyyy hh:mm
     SimpleDateFormat simpleDateFormat = new SimpleDateFormat("MM/dd/yyyy HH:mm");
